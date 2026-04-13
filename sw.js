050f6b842581d716b81658d75afb4cd15116be74
sgccofficial <sujithprasanth21@gmail.com>
2026-04-13 20:47:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "habit-app-v2";
+const CACHE_NAME = "habitflow-v0";
 
 const urlsToCache = [
   "./",
@@ -6,23 +6,37 @@ const urlsToCache = [
   "journal.html",
   "timer.html",
   "manifest.json",
-  "icon.png"
+  "icon-192.png",
+  "icon-512.png"
 ];
 
-// Install
+// INSTALL
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch
+// ACTIVATE (clear old cache)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH (network first, fallback to cache)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(res => res)
+      .catch(() => caches.match(event.request))
   );
 });
